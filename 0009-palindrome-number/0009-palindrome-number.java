ef35af447eed3cb8284efbd6b648e37ac14a47b2
Hari Om Gautam <gautam.hariom25@gmail.com>
2023-12-06 12:45:20 +0530

    Time: 9 ms (11.32%), Space: 43.2 MB (21.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+          int y=x;
+          List<Integer> list = new ArrayList();
+          do {
+              list.add(x%10);
+              x=x/10;
+
+          }while(x!=0);
+        return y==result(list) && y>=0?true:false;
+
+
+    }
+
+    public int result(List< Integer > list)
+    {
+        double x=0;
+        int i=0;
+        do {
+            x = (list.get(i) * Math.pow(10, list.size() - 1 - i)) + x;
+            if(x>Integer.MAX_VALUE || x<Integer.MIN_VALUE)
+            {
+                return 0;
+            }
+           i++;
+        }while(i<list.size());
+        return (int) x;
+    }
+}
