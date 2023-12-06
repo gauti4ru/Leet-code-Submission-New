0b85dbfb95a9ed6a5b476050d3da6af10e01fa07
Hari Om Gautam <gautam.hariom25@gmail.com>
2023-12-06 12:39:41 +0530

    Time: 2 ms (16.63%), Space: 40 MB (29.94%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.pow;
+class Solution {
+   public int reverse(int x) {
+          int y=x;
+          List<Integer> list = new ArrayList();
+          do {
+              list.add(x%10);
+              x=x/10;
+
+          }while(x!=0);
+        return y<=Integer.MAX_VALUE && y>=Integer.MIN_VALUE ? result(list):0;
+
+
+    }
+
+    public int result(List< Integer > list)
+    {
+        double x=0;
+        int i=0;
+        do {
+            x = (list.get(i) * Math.pow(10, list.size() - 1 - i)) + x;
+            if(x>Integer.MAX_VALUE || x<Integer.MIN_VALUE)
+            {
+                return 0;
+            }
+           i++;
+        }while(i<list.size());
+        return (int) x;
+    }
+
+
+
+}
