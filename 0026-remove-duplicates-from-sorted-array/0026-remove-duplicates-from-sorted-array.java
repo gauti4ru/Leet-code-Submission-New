6cd5a55ffa30376c3f120dfe623c2673b2d4c59c
Hari Om Gautam <gautam.hariom25@gmail.com>
2023-12-14 00:59:04 +0530

    Update 0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -27,4 +27,26 @@ class Solution {
 
         }
     }
+
+    //More Optimised solution using pointer
+    /*
+
+  public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+
+        int left = 0;
+        int right = 1;
+
+        while (right < len) {
+            if (nums[right] > nums[left]) {
+                left++;
+                nums[left] = nums[right];
+            }
+
+            right++;
+        }
+
+        return left + 1;
+    }
+    */
 }
