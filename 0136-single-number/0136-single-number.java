bbbcccc58e828202d5b88c6d7e863b95a80f1d0c
Hari Om Gautam <gautam.hariom25@gmail.com>
2023-12-26 17:26:25 +0530

    Time: 1 ms (99.93%), Space: 46.8 MB (5.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int num : nums)  result ^= num;
+        return result;
+            
+    }
+}
+/*
+ We are using the concept of XOR Bit wise manipulation
+ 
+ XOR Propetry
+ 
+ A XOR A = 0
+ A XOR 1 = A
+ A XOR 0 = A
+ 
+ :- A XOR B XOR C XOR = element which is single will be the output
+    */
