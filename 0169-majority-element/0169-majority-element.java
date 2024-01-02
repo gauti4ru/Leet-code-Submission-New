5cd48915827b6cd2a69bd28aab2cfbf3474d1d8c
Hari Om Gautam <gautam.hariom25@gmail.com>
2024-01-02 16:45:53 +0530

    Time: 2212 ms (5.02%), Space: 50.2 MB (13.45%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int count = 0,i=0,maxcount=0,majorElement=0;
+        while(i<=nums.length/2){
+
+            for(int j=i;j<nums.length;j++){
+                if(nums[i]==nums[j]) count++;
+            }
+            majorElement=maxcount<count ?nums[i] : majorElement;
+            maxcount=maxcount<count ?count : maxcount;
+            i++;
+            count=0;
+            
+        }
+        return nums.length==1?nums[0]:majorElement;
+    }
+}
+
+
+
+
+
+/*
+#Boyer-Morre Majority Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int  vote=0;
+        int candidate=0;
+        int count=0;
+        for(int num : nums){
+            if(vote==0) {
+            candidate=num;
+            vote++;
+            }
+            else{
+                if(num==candidate) vote++;
+                else vote--;
+            }
+        }
+        
+        for(int num :nums){
+            if(num==candidate) count++;
+        }
+        return count>nums.length/2 ? candidate :-1;
+        
+}
+}
+
+*/
+
+/*
+#Solution using Sort method of Arrays class
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
+
+*/
