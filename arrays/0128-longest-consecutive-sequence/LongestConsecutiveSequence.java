9c57821a4b3bbb0ac5a89475a2e4f7bc0b457509
gauti4ru <gautam.hariom25@gmail.com>
2024-01-09 19:07:51 +0530

    Class Name Corrected

--- arrays/0128-longest-consecutive-sequence/LongestConsecutiveSequence.java
+++ arrays/0128-longest-consecutive-sequence/LongestConsecutiveSequence.java
@@ -1,8 +1,8 @@
 //First Solution is based on sorting the array and then. finding the consecutive numbers.
 import java.util.Arrays;
+import java.util.HashSet;
 class LongestConsecutiveSequence {
     public int longestConsecutive(int[] nums) {
-        //Inserion sort is isncreasing the compleixty so thats why using default sort method
         Arrays.sort(nums);
         int count = 0;
         int temp = nums.length == 0 ? 0 : 1;
@@ -16,22 +16,36 @@ class LongestConsecutiveSequence {
         }
         return temp > count ? temp : count;
     }
-    /*
-    Code for Inserion sort 
-    public void insertionSort(int[] nums){
-        for(int i=1;i<nums.length;i++){
-            int j=i-1;
-            while(j>=0){
-                if(nums[j]>nums[i]){
-                    int temp=nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                    i=j;
-                    }
-                else break;
-                j--;
+
+    //Best Case Approach with O(n) complexity.
+    public static int longestConsecutive01(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        // Create a HashSet to store the elements of the array
+        //Creating a HashSet is necessary to use for simplifying the searching algorithm.
+        HashSet< Integer > set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int maxLength = 0;
+        // Iterate through the array
+        for(int num : nums) {
+            // Check if num is the start of a sequence
+            if(!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+                // Increment currentNum and update currentLength while consecutive elements are present
+                while(set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+                // Update maxLength if the current sequence is longer
+                maxLength = Math.max(maxLength, currentLength);
             }
         }
+        return maxLength;
     }
-    */
 }
+
+
