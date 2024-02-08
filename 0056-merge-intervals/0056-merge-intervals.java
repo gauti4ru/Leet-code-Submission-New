bd1794c6bea6735840c0d5aa6c77637f1a1ad3b2
Hari Om Gautam <gautam.hariom25@gmail.com>
2024-02-08 15:20:18 +0530

    Time: 9 ms (60.66%), Space: 46.1 MB (83.27%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public static int[][] merge(int[][] intervals) {
+        if(intervals.length == 1)
+            return intervals;
+        Arrays.sort(intervals, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        List< int[] > mergedList = new ArrayList<>();
+        int j = 0;
+        for(int i = 0; i < intervals.length; i++) {
+            int[] mergedArray = intervals[i];
+            for(j = i + 1; j < intervals.length; j++) {
+                if((mergedArray[1] >= intervals[j][0])) {
+                    mergedArray[1] = Math.max(intervals[j][1], mergedArray[1]);
+                } else {
+                    break;
+                }
+            }
+            i = j - 1;
+            mergedList.add(mergedArray);
+        }
+        //Converting Set into array
+        int[][] mergedOutputArray = new int[mergedList.size()][2];
+        for(int i = 0; i < mergedList.size(); i++) {
+            mergedOutputArray[i] = mergedList.get(i);
+        }
+        return mergedOutputArray;
+    }
+}
